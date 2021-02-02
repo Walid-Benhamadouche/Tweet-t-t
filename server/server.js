4fe22dd1a240b46f44441743f53d65014dbb9f32
Valar <drakkendusk@gmail.com>
2021-02-02 23:31:10 +0100

    adding deploy config

--- server/server.js
+++ server/server.js
@@ -48,7 +48,12 @@ store.on('error', function(error){
 })
 
 app.use(enforce.HTTPS({ trustProtoHeader: true }))
-app.use(serveStatic(__dirname + '/dist'))
+if (process.env.NODE_ENV === "production"{
+    app.use(express.static("build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+    });
+  }
 app.use(history())
 
 app.use(cors({
