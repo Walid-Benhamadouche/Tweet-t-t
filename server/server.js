51406efdb7669397667d0d6e5b894e13b172eac2
Valar <drakkendusk@gmail.com>
2021-02-02 23:57:09 +0100

    adding deploy config

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const app = require('express')()
-const serveStatic = require('serve-static')
+//const serveStatic = require('serve-static')
 const history = require('connect-history-api-fallback')
 const enforce = require('express-sslify')
 const cors = require('cors')
@@ -48,7 +48,8 @@ store.on('error', function(error){
 })
 
 app.use(enforce.HTTPS({ trustProtoHeader: true }))
-app.use(serveStatic(__dirname + '/dist'))
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir))
 app.use(history())
 
 app.use(cors({
