b9102571261b91b273eb0dbbc7f4f8530f4c0e74
Valar <drakkendusk@gmail.com>
2021-02-02 23:03:41 +0100

    adding deploy config

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -4,72 +4,72 @@ axio.defaults.withCredentials = true
 const baseURL = process.env.baseURL || "http://localhost:5000"
 export default {
     async auth(){
-        let auth = await axio.get(baseURL+"/users/auth")
+        let auth = await axio.get(process.env.baseURL+"/users/auth")
         console.log(auth.data.loggedIn)
         return auth.data
     },
     async getUser(){
-        let user = await axio.get(baseURL+"/users/getUser");
+        let user = await axio.get(process.env.baseURL+"/users/getUser");
         return user.data
     },
     async getUserId(data){
         console.log("this is the sent data", data)
-        let user = await axio.get(baseURL+"/users/getUserId", {params: data});
+        let user = await axio.get(process.env.baseURL+"/users/getUserId", {params: data});
         return user.data
     },
     async logIn(data){
         console.log(data)
-        let user = await axio.get(baseURL+"/users/login", {params: data});
+        let user = await axio.get(process.env.baseURL+"/users/login", {params: data});
         console.log(user.data)
         return user.data
     },
     async signUp(data){
-        let user = await axio.post(baseURL+"/users/signup", null, {params: data});
+        let user = await axio.post(process.env.baseURL+"/users/signup", null, {params: data});
         return user.data
     },
     async Tweet(data){
-        let tweet = await axio.post(baseURL+"/tweet/tweet", null, {params: data})
+        let tweet = await axio.post(process.env.baseURL+"/tweet/tweet", null, {params: data})
         return tweet.data
     },
     async getTweets(data){
-        let tweets = await axio.get(baseURL+"/tweet/getTweets", {params: data})
+        let tweets = await axio.get(process.env.baseURL+"/tweet/getTweets", {params: data})
         return tweets.data
     },
     async follow(data){
-        let follow = await axio.post(baseURL+"/follow/follow", null, {params: data})
+        let follow = await axio.post(process.env.baseURL+"/follow/follow", null, {params: data})
         return follow.data
     },
     async getFollowing(data){
-        let following = await axio.get(baseURL+"/follow/getFollowings", {params: data})
+        let following = await axio.get(process.env.baseURL+"/follow/getFollowings", {params: data})
         return following.data
     },
     async getFollower(data){
-        let follower = await axio.get(baseURL+"/follow/getFollowers", {params: data})
+        let follower = await axio.get(process.env.baseURL+"/follow/getFollowers", {params: data})
         return follower.data
     },
     async unFollow(data){
-        let unfollow = await axio.delete(baseURL+"/follow/unfollow", {params: data})
+        let unfollow = await axio.delete(process.env.baseURL+"/follow/unfollow", {params: data})
         console.log(unfollow.data)
         return unfollow.data
     },
     async makeChatRoom(data){
-        let room = await axio.post(baseURL+"/chat/Chat", null, {params: data})
+        let room = await axio.post(process.env.baseURL+"/chat/Chat", null, {params: data})
         return room.data
     },
     async getChatRoom(data){
-        let room = await axio.get(baseURL+"/chat/getChat", {params: data})
+        let room = await axio.get(process.env.baseURL+"/chat/getChat", {params: data})
         return room.data
     },
     async getMessages(data){
-        let messages = await axio.get(baseURL+"/chat/getMessages", {params: data})
+        let messages = await axio.get(process.env.baseURL+"/chat/getMessages", {params: data})
         return messages.data
     },
     async sendMessage(data){
-        let message = await axio.post(baseURL+"/chat/sendMessage", null, {params: data})
+        let message = await axio.post(process.env.baseURL+"/chat/sendMessage", null, {params: data})
         return message.data
     },
     async updateCover(data){
-        let user = await axio.put(baseURL+"/users/update", {data})
+        let user = await axio.put(process.env.baseURL+"/users/update", {data})
         return user.data
     }
 }
