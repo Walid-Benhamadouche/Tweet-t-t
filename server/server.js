0445f7343da3c50f5717985dc9b0ca70b92a38e7
Valar <drakkendusk@gmail.com>
2021-05-06 22:16:28 +0100

    changes for heroku

--- server/server.js
+++ server/server.js
@@ -11,12 +11,12 @@ const MongoDBStore = require('connect-mongodb-session')(session)
 require('dotenv/config')
 const genuuid = require('uuid/v4');
 
-if (process.env.NODE_ENV === "production"){
-    app.use(express.static("build"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-    });
-  }
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 mongoose.connect(process.env.DB_CONNECTION,{ useNewUrlParser: true,useUnifiedTopology: true },()=>{console.log("connected to mongo")})
 
