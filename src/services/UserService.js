451a35fd668c70e3efe1fe3b86c2e21cdcd97351
Valar <drakkendusk@gmail.com>
2021-05-06 22:24:25 +0100

    changes for heroku

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -3,35 +3,35 @@ import axio from "axios"
 axio.defaults.withCredentials = true
 export default {
     async auth(){
-        let auth = await axio.get("http://localhost:5000/users/auth")
+        let auth = await axio.get("https://vuejslearningapp.herokuapp.com:process.env.PORT/users/auth")
         console.log(auth.data.loggedIn)
         return auth.data
     },
     async getUser(){
-        let user = await axio.get("http://localhost:5000/users/getUser");
+        let user = await axio.get("https://vuejslearningapp.herokuapp.com:process.env.PORT/users/getUser");
         return user.data
     },
     async getUserId(data){
         console.log("this is the sent data", data)
-        let user = await axio.get("http://localhost:5000/users/getUserId", {params: data});
+        let user = await axio.get("https://vuejslearningapp.herokuapp.com:process.env.PORT/users/getUserId", {params: data});
         return user.data
     },
     async logIn(data){
         console.log(data)
-        let user = await axio.get("http://localhost:5000/users/login", {params: data});
+        let user = await axio.get("https://vuejslearningapp.herokuapp.com:process.env.PORT/users/login", {params: data});
         console.log(user.data)
         return user.data
     },
     async signUp(data){
-        let user = await axio.post("http://localhost:5000/users/signup", null, {params: data});
+        let user = await axio.post("https://vuejslearningapp.herokuapp.com:process.env.PORT/users/signup", null, {params: data});
         return user.data
     },
     async Tweet(data){
-        let tweet = await axio.post("http://localhost:5000/tweet/tweet", null, {params: data})
+        let tweet = await axio.post("https://vuejslearningapp.herokuapp.com:process.env.PORT/tweet/tweet", null, {params: data})
         return tweet.data
     },
     async getTweets(data){
-        let tweets = await axio.get("http://localhost:5000/tweet/getTweets", {params: data})
+        let tweets = await axio.get("https://vuejslearningapp.herokuapp.com:process.env.PORT/tweet/getTweets", {params: data})
         return tweets.data
     },
     async follow(data){
